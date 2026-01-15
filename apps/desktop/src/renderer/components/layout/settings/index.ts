15287ff38b9fc7574da8591603bf675809782fa3
DanielScharfsteinGit <daniel.scharfstein@accomplish.ai>
2026-01-15 18:55:33 +0200

    feat: add index file for settings components

---
+++ apps/desktop/src/renderer/components/layout/settings/index.ts
@@ -0,0 +1,7 @@
+export { default as ChooseModelType } from './ChooseModelType';
+export { default as SelectProvider } from './SelectProvider';
+export { default as AddApiKey } from './AddApiKey';
+export { default as SelectModel } from './SelectModel';
+export { default as OllamaSetup } from './OllamaSetup';
+export { default as ApiKeysSection } from './ApiKeysSection';
+export * from './types';
